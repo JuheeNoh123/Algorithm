f0dec5aad467f6339df018ff9b795549946668c6
JuheeNoh <127907793+JuheeNoh123@users.noreply.github.com>
2024-12-04 21:54:23 +0900

    [Silver I] Title: 쿠키크루, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/22939. 쿠키크루/쿠키크루.py
@@ -0,0 +1,41 @@
+import sys
+from itertools import permutations
+input = sys.stdin.readline
+
+n = int(input())
+field = [[] for _ in range(n)]
+crew = ['Assassin','Healer','Mage','Tanker']
+toppingKinds= ['J','C','B','W']
+home = [0,0]
+cookie = [0,0]
+topping = [[] for _ in range(4)]
+
+def dist(a,b):
+    #print(a,b)
+    return abs(a[0]-b[0])+abs(a[1]-b[1])
+
+
+for i in range(n):
+    field[i] = input().strip()
+    for j in range(n):
+        if field[i][j]=="H":
+            home = [i,j]
+        if field[i][j]=="#":
+            cookie = [i,j]
+        for k in range(4):
+            if field[i][j]==toppingKinds[k]:
+                topping[k].append([i,j])
+    min = 10000
+    type = 0
+    for k in range(4):
+        perm = permutations(topping[k],3)
+        
+        for p in perm:
+            #print(p)
+            d = dist(home,p[0])+dist(p[0],p[1])+dist(p[1],p[2])+dist(p[2],cookie)
+            if min>d:
+                min = d
+                type = k
+            #print(d, type)
+        
+print(crew[type])
