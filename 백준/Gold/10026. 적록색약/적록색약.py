8a9dd3424d22ae2d7a2697678c7f531415efbbc6
JuheeNoh123 <127907793+JuheeNoh123@users.noreply.github.com>
2025-01-10 17:23:08 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,44 +1,53 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-def BFS(x,y):
-    q.append((x,y))
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
-    visited[x][y] = 1
-    while q:
-        x, y = q.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if 0<=nx<N and 0<=ny<N and a[nx][ny] == a[x][y] and not visited[nx][ny]:
-                visited[nx][ny] = 1 
-                q.append((nx,ny))
+n = int(input())
+graph = []
+for i in range(n):
+    L = list(input().strip())
+    graph.append(L)
+    
+visited = [[0 for _ in range(n)] for _ in range(n)]
+
+def bfs(sx, sy, color):
+    dx, dy = [1,-1,0,0],[0,0,1,-1]
+    queue = deque([(sx,sy)])
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0<=nx<n and 0<=ny<n and not visited[nx][ny]:
+                
+                if graph[nx][ny]==color:
+                    visited[nx][ny]=1
+                    queue.append((nx,ny))
+
+    return visited
 
 
-N = int(input())
-a = [list(input()) for _ in range(N)]
-q = deque()
+                        
+                        
 
-# 적록색약 아닌 경우
-visited = [[0] * N for _ in range(N)]
-cnt1 = 0
-for i in range(N):
-    for j in range(N):
-        if not visited[i][j]: 
-            BFS(i,j)
-            cnt1 += 1
-
-for i in range(N):
-    for j in range(N):
-        if a[i][j] == 'G':
-            a[i][j] = 'R'
-
-visited = [[0] * N for _ in range(N)]
-cnt2 = 0
-for i in range(N):
-    for j in range(N):
+    return visited
+cnt =0
+for i in range(n):
+    for j in range(n):
         if not visited[i][j]:
-            BFS(i,j)
-            cnt2 += 1
+            bfs(i,j,graph[i][j])
+            cnt += 1
+print(cnt, end=' ')
 
-print(cnt1, cnt2)
+cnt=0
+visited = [[0 for _ in range(n)] for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if graph[i][j]=='G':
+            graph[i][j]='R'
+            
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(i,j,graph[i][j])
+            cnt += 1
+print(cnt)
