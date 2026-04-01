ae4dc5281441af511358a5cc727cfda7192b5e77
JuheeNoh123 <127907793+JuheeNoh123@users.noreply.github.com>
2026-04-02 03:31:20 +0900

    [Silver III] Title: 눈 치우기, Time: 88 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/26215. 눈 치우기/눈 치우기.py
@@ -0,0 +1,15 @@
+a = int(input())
+L = list(map(int, input().split()))
+cnt = 0
+while sum(L) != 0:
+    L.sort(reverse=True)
+    L[0] -= 1
+    if len(L)>=2 and L[1] != 0:
+        L[1] -= 1
+    
+    cnt += 1
+    #print(L)
+if cnt > 1440:
+    print(-1)
+else:
+    print(cnt)
