1387606f8f2f2d23c93cdaef5dfe449a3c233d33
JuheeNoh123 <127907793+JuheeNoh123@users.noreply.github.com>
2026-05-18 18:59:30 +0900

    [level 2] Title: 짝지어 제거하기, Time: 89.67 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = 0
+    stack = []
+    for i in s:
+        if len(stack)!=0 and stack[-1]==i:
+            stack.pop()
+        else:
+            stack.append(i)
+    if len(stack)==0:
+        answer=1
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+   # print('Hello Python')
+
+    return answer
