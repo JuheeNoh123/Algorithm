1dcb86ea1b509fec53dbfac1c5daeec9aee86085
JuheeNoh123 <127907793+JuheeNoh123@users.noreply.github.com>
2026-06-15 16:18:15 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,7 @@
 def solution(array, commands):
-    result = []
-    for i in commands:
-        ans = array[i[0]-1:i[1]]
-        ans.sort()
-        result.append(ans[i[2]-1])
-    print(result)
-    return result 
+    answer = []
+    for i, j, k in commands:
+        cut = array[i-1:j]
+        cut.sort()
+        answer.append(cut[k-1])
+    return answer
